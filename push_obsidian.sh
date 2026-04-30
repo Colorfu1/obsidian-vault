2d75d5c8bd8477ada145bdb395736990b0ee67f6
wuwenda <wuwenda@xiaomi.com>
2026-04-30 20:50:12 +0800

    Add push on phone 0430 2050
    Signed-off-by: wuwenda <wuwenda@xiaomi.com>

--- push_obsidian.sh
+++ push_obsidian.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
+
 echo "== Push Obsidian vault =="
+
 lg2 add .
-msg="update from iphone $(date '+%Y-%m-%d %H:%M:%S')"
+lg2 status
+
+msg="update from iphone $(date "+%Y-%m-%d %H:%M:%S")"
+
 lg2 commit -m "$msg"
-lg2 push origin
-echo "== Push done =="
+
+if [ $? -eq 0 ]; then
+  lg2 push origin
+  echo "== Push done =="
+else
+  echo "== No commit created, skip push =="
+fi
